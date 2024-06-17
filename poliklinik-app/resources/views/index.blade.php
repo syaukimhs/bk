b7e36c6f92f47ec8f7a65163cd89572a4e2aa647
syaukimhs <92071829+syaukimhs@users.noreply.github.com>
2024-06-17 21:58:00 +0700

    Update index.blade.php

--- poliklinik-app/resources/views/index.blade.php
+++ poliklinik-app/resources/views/index.blade.php
@@ -11,35 +11,36 @@
     <title>index</title>
   </head>
   <body>
-  <div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="/login" class="btn btn-primary">Login admin</a>
+    <div class="container">   
+      <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="/login" class="btn btn-primary">Login admin</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="/logindokter" class="btn btn-primary">Login dokter</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="/loginpasien" class="btn btn-primary">Login pasien</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="/logindokter" class="btn btn-primary">Login dokter</a>
-      </div>
     </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="/loginpasien" class="btn btn-primary">Login pasien</a>
-      </div>
-    </div>
-  </div>
-</div>
-
   </body>
 </html>
